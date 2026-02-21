fec9b03fcbf9f9e47151764359b9f247c43edd3c
KandakatlaRithwik <rithwikkandakatla@gmail.com>
2026-02-21 15:31:30 +0530

    updated

--- index8a.js
+++ index8a.js
@@ -3,20 +3,18 @@ const app = express();
 app.listen(8080);
 app.use(express.json())
 const products = [
-  { id: 1, name: "Ball", price: 500 },
-  { id: 2, name: "Bat", price: 500000 },
-  { id: 3, name: "wickets", price: 200 },
+  { id: 1, name: "DELL", price: 50000 },
+  { id: 2, name: "HP", price: 50000 },
+  { id: 3, name: "ASUS", price: 80000 },
 ];
 app.get("/", (req, res) => {
   res.json(products);
 });
-
 app.get("/:id", (req, res) => {
   const id = Number(req.params.id);
   const result = products.find((product) => product.id === id);
   res.json(result);
 });
-
 app.post("/", (req, res) => {
   const product = req.body;
   products.push(product);
